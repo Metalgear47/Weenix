66fb436d51d11459f7b4e68d08204b300c307683
Luqi Pan <luqi.pan@gmail.com>
2014-04-15 16:44:18 -0400

    s5fs_lookup

--- kernel/fs/s5fs/s5fs.c
+++ kernel/fs/s5fs/s5fs.c
@@ -551,8 +551,21 @@ s5fs_mknod(vnode_t *dir, const char *name, size_t namelen, int mode, devid_t dev
 int
 s5fs_lookup(vnode_t *base, const char *name, size_t namelen, vnode_t **result)
 {
-        NOT_YET_IMPLEMENTED("S5FS: s5fs_lookup");
-        return -1;
+    KASSERT(base);
+    KASSERT(name);
+    KASSERT(result);
+    KASSERT(namelen <= S5_NAME_LEN);
+
+    int inodeno = s5_find_dirent(base, name, namelen);
+    if (inodeno < 0) {
+        return inodeno;
+    }
+    KASSERT(inodeno);
+
+    *result = vget(base->vn_fs, inodeno);
+    return 0;
+        /*NOT_YET_IMPLEMENTED("S5FS: s5fs_lookup");*/
+        /*return -1;*/
 }
 
 /*
